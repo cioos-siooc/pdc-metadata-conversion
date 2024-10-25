e8f7c995405ff640ce841d31b99c449d19175860
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2024-10-25 15:53:54 -0400

    add iso test

--- tests/test_pdc.py
+++ tests/test_pdc.py
@@ -3,9 +3,40 @@ from glob import glob
 import pytest
 
 import pdc.fgdc as fgdc
+from pdc.iso import PDC_ISO
 
 
 @pytest.mark.parametrize("file", glob("tests/files/pdc*fgdc.xml"))
 def test_fgdc(file):
-    result = fgdc.main(file, "userID", "filename", "test-recordID", "status" , "CC-BY-4.0", "region", ["dataset"], ["sharedWith"])
+    result = fgdc.main(
+        file,
+        "userID",
+        "filename",
+        "test-recordID",
+        "status",
+        "CC-BY-4.0",
+        "region",
+        ["dataset"],
+        ["sharedWith"],
+    )
+    assert result
+
+
+@pytest.mark.parametrize("file", glob("tests/files/pdc*iso.xml"))
+def test_iso_xml(file):
+    pdc_iso = PDC_ISO(file)
+    result = pdc_iso.to_cioos(
+        "userID",
+        "filename",
+        "test-recordID",
+        "status",
+        "CC-BY-4.0",
+        "region",
+        [],
+        ["dataset"],
+        ["sharedWith"],
+        [],
+        ["eov"],
+        "identifier",
+    )
     assert result
